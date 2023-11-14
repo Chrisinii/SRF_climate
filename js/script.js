a7fe3ae24ff5e2396a9d82878082d29e3758e08e
Chrisinii <christina.benz@stud.fhgr.ch>
2023-11-15 00:35:28 +0100

    Daten anzeigen

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import { key } from './key.js'
 
-
+// Todo: Alle gewünschten Daten aus API rausholen lassen. Diese dann mit Flexbox anzeigen lassen.
 export const fetchAccessToken = async() => {
     try {
         const accessRequest = await fetch("https://api.srgssr.ch/oauth/v1/accesstoken?grant_type=client_credentials", {
@@ -21,29 +21,25 @@ export const fetchAccessToken = async() => {
     }
 }
 
+// API Request 20230101 - 20230430
 fetchAccessToken().then(token => {
     fetchAPI(token).then(response => {
-        console.log(response.data.articles.edges);
+        // console.log(response.data.articles.edges);
 
         const klimaArtikel = response.data.articles.edges.filter(article => {
             return JSON.stringify(article).includes("Klima");
         })
 
         console.log(klimaArtikel);
-    }).catch(error => {
-        console.error('API Fetch Error:', error);
-    });
-});
 
-fetchAccessToken().then(token => {
-    fetchAPITwo(token).then(response => {
-        console.log(response.data.articles.edges);
+        const datenAnzeigeElement = document.getElementById('datenAnzeige');
 
-        const klimaArtikel = response.data.articles.edges.filter(article => {
-            return JSON.stringify(article).includes("Klima");
-        })
+        klimaArtikel.forEach(article => {
+            const titelElement = document.createElement('p');
+            titelElement.textContent = article.title[0].content;
+            datenAnzeigeElement.appendChild(titelElement);
+        });
 
-        console.log(klimaArtikel);
     }).catch(error => {
         console.error('API Fetch Error:', error);
     });
@@ -53,7 +49,7 @@ fetchAccessToken().then(token => {
 
 export const fetchAPI = async(accessToken) => {
     try {
-        const NewsRequestOne = await fetch("https://api.srgssr.ch/srgssr-contents/v1/articles?items=500&bu=SRF&genre=News%2C%20Schweiz%2C%20General&sort=releasedAt&order=DESC&publishedDateFrom=2023-01-01T00%3A00%3A00Z&publishedDateTo=2023-06-30T00%3A00%3A00Z", {
+        const NewsRequestOne = await fetch("https://api.srgssr.ch/srgssr-contents/v1/articles?items=500&bu=SRF&genre=News%2C%20Schweiz%2C%20General&sort=releasedAt&order=DESC&publishedDateFrom=2023-01-01T00%3A00%3A00Z&publishedDateTo=2023-04-30T00%3A00%3A00Z", {
             method: 'GET',
             headers: new Headers({
                 'Authorization': `Bearer ${accessToken}`,
@@ -76,9 +72,24 @@ export const fetchAPI = async(accessToken) => {
 
 }
 
+// API Request 20230501 - 20230830
+fetchAccessToken().then(token => {
+    fetchAPITwo(token).then(response => {
+        // console.log(response.data.articles.edges);
+
+        const klimaArtikel = response.data.articles.edges.filter(article => {
+            return JSON.stringify(article).includes("Klima");
+        })
+
+        console.log(klimaArtikel);
+    }).catch(error => {
+        console.error('API Fetch Error:', error);
+    });
+});
+
 export const fetchAPITwo = async(accessToken) => {
     try {
-        const NewsRequestTwo = await fetch("https://api.srgssr.ch/srgssr-contents/v1/articles?items=500&bu=SRF&genre=News%2C%20Schweiz%2C%20General&sort=releasedAt&order=DESC&publishedDateFrom=2023-07-01T00%3A00%3A00Z&publishedDateTo=2023-12-31T00%3A00%3A00Z", {
+        const NewsRequestTwo = await fetch("https://api.srgssr.ch/srgssr-contents/v1/articles?items=500&bu=SRF&genre=News%2C%20Schweiz%2C%20General&sort=releasedAt&order=DESC&publishedDateFrom=2023-05-01T00%3A00%3A00Z&publishedDateTo=2023-08-31T00%3A00%3A00Z", {
             method: 'GET',
             headers: new Headers({
                 'Authorization': `Bearer ${accessToken}`,
@@ -101,85 +112,75 @@ export const fetchAPITwo = async(accessToken) => {
 
 }
 
+// API Request 20230901 - 20231231
+fetchAccessToken().then(token => {
+    fetchAPIThree(token).then(response => {
+        // console.log(response.data.articles.edges);
 
-// export const fetchAPI = async(accessToken) => {
-//     try {
-//         const NewsRequest = await fetch("https://api.srgssr.ch/srgssr-contents/v1/articles?items=500&bu=SRF&genre=News&sort=releasedAt&order=DESC&publishedDateFrom=2023-01-01T00%3A00%3A00Z&publishedDateTo=2023-12-31T00%3A00%3A00Z", {
-//             method: 'GET',
-//             headers: new Headers({
-//                 'Authorization': `Bearer ${accessToken}`,
-//             }), 
+        const klimaArtikel = response.data.articles.edges.filter(article => {
+            return JSON.stringify(article).includes("Klima");
+        })
 
-            
-//         }).catch(error => {
-//             console.error('Fetch Error:', error);
-//             return {error};
-//         }); 
-        
+        console.log(klimaArtikel);
+    }).catch(error => {
+        console.error('API Fetch Error:', error);
+    });
+});
 
-//         if(NewsRequest.error) {
-//             return NewsRequest;
-//         }
+export const fetchAPIThree = async(accessToken) => {
+    try {
+        const NewsRequestThree = await fetch("https://api.srgssr.ch/srgssr-contents/v1/articles?items=500&bu=SRF&genre=News%2C%20Schweiz%2C%20General&sort=releasedAt&order=DESC&publishedDateFrom=2023-09-01T00%3A00%3A00Z&publishedDateTo=2023-12-31T00%3A00%3A00Z", {
+            method: 'GET',
+            headers: new Headers({
+                'Authorization': `Bearer ${accessToken}`,
+            }), 
+        }).catch(error => {
+            console.error('Fetch Error:', error);
+            return {error};
+        });
 
-//         console.log(NewsRequest)
-//         return await NewsRequest.json();
-//     } catch(error) {
-//         console.error('Error:', error);  // Log the error to the console
-//         return { error: { message: 'Error Fetching Data'}};
-//     }
+        if(NewsRequestThree.error) {
+            return NewsRequestThree;
+        }
 
-    
-// }
+        console.log(NewsRequestThree)
+        return await NewsRequestThree.json();
+    } catch(error) {
+        console.error('Error:', error);  // Log the error to the console
+        return { error: { message: 'Error Fetching Data'}};
+    }
 
+}
 
+// // Daten anzeigen
 
-// export const fetchAPI = async(accessToken) => {
-//     try {
-//         const NewsRequestOne = await fetch("https://api.srgssr.ch/srgssr-contents/v1/articles?items=500&bu=SRF&genre=News%2C%20General%2C%20History%2C%20Events%2C%20Sport&sort=releasedAt&order=DESC&publishedDateFrom=2023-01-01T00%3A00%3A00Z&publishedDateTo=2023-06-30T00%3A00%3A00Z", {
-//             method: 'GET',
-//             headers: new Headers({
-//                 'Authorization': `Bearer ${accessToken}`,
-//             }), 
-//         }).catch(error => {
-//             console.error('Fetch Error:', error);
-//             return {error};
-//         });
+// const apiData = [
+//     // Hier kommt deine API-Daten-Array
+//     // ...
+// ];
 
-//         if(NewsRequestOne.error) {
-//             return NewsRequestOne;
-//         }
+// const container = document.querySelector(".container");
 
-//         console.log(NewsRequestOne)
-//         return await NewsRequestOne.json();
-//     } catch(error) {
-//         console.error('Error:', error);  // Log the error to the console
-//         return { error: { message: 'Error Fetching Data'}};
-//     }
+// apiData.forEach(data => {
+//     const card = document.createElement("div");
+//     card.classList.add("card");
 
+//     const title = document.createElement("h2");
+//     title.textContent = data.title;
 
-//     try {
-//         const NewsRequestTwo = await fetch("https://api.srgssr.ch/srgssr-contents/v1/articles?items=500&bu=SRF&genre=News%2C%20General%2C%20History%2C%20Events%2C%20Sport&sort=releasedAt&order=DESC&publishedDateFrom=2023-07-01T00%3A00%3A00Z&publishedDateTo=2023-12-31T00%3A00%3A00Z", {
-//             method: 'GET',
-//             headers: new Headers({
-//                 'Authorization': `Bearer ${accessToken}`,
-//              }), 
-            
-//         }).catch(error => {
-//             console.error('Fetch Error:', error);
-//             return {error};
-//         }); 
-        
+//     const lead = document.createElement("p");
+//     lead.textContent = data.lead[0].content;
 
-//         if(NewsRequestTwo.error) {
-//             return NewsRequestTwo;
-//         }
+//     const content = document.createElement("p");
+//     content.textContent = data.content[0].content;
 
-//         console.log(NewsRequestTwo)
-//         return await NewsRequestTwo.json();
-//     } catch(error) {
-//         console.error('Error:', error);  // Log the error to the console
-//         return { error: { message: 'Error Fetching Data'}};
-//     }
+//     const released = document.createElement("p");
+//     released.textContent = `Veröffentlicht am: ${data.releasedAt}`;
 
-    
-// }
+//     card.appendChild(title);
+//     card.appendChild(lead);
+//     card.appendChild(content);
+//     card.appendChild(released);
+
+//     container.appendChild(card);
+// });
